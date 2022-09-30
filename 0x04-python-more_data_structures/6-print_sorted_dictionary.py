06f3d4dfeac61bae03f9809d16202c3888d4b7bc
FelixOtienoArogo <felixarogo@gmail.com>
2022-09-30 22:16:31 +0300

    tow

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for i in sorted(a_dictionary):
+        print("{0}: {1}".format(i, a_dictionary[i]))
+    return sorted(a_dictionary)
