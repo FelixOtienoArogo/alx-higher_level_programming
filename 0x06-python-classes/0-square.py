005327ea4418bdf087a13df6cbd7619ca2c92b34
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-13 20:45:15 +0300

    siquare

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""First square."""
 class Square:
+    """
+    This is a square class.
+    
+    ...
+    
+    Attributes
+    ----------
+    None
+    
+    Methods
+    -------
+    None
+
+    """
+    
     pass
