7e4e0fec108881d1b6bef86bbf290875d3a9dd96
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-13 22:33:14 +0300

    he

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""Fourth square."""
+
+
+class Square:
+    """
+    This is a square class.
+
+    ...
+
+    Attributes
+    ----------
+    __size:
+        this is the size of the square
+
+    Methods
+    -------
+    None
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Parameters.
+
+        ----------
+
+        size: int
+
+            The size of the square
+
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Paramaneters.
+
+        ------------
+
+        None
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Parameters.
+
+        ----------
+
+        None
+
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+
+        Paramaneters.
+
+        ------------
+
+        None
+
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Parameters.
+
+        ----------
+
+        None
+
+        """
+        if (self.__size == 0):
+            print("")
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
