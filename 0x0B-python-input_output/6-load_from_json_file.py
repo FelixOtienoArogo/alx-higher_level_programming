a8885fe802059df74fee37c1e4525f2f87e3848d
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-27 05:24:31 +0300

    load

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-
+"""to_json_string.py"""
 
 import json
 
 
 def load_from_json_file(filename):
+    """
+    Parameters.
+
+    ----------
+
+    size: int
+
+       The size of the square
+
+    """
     with open(filename, "r", encoding="UTF-8") as f:
         return json.load(f)
