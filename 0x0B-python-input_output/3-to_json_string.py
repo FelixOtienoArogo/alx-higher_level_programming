db2394ac067d5ee3be2f285b7a11abb1a48ff46d
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-27 05:05:37 +0300

    type

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-
+"""to_json_string.py"""
 
 import json
 
 
 def to_json_string(my_obj):
+    """
+    Parameters.
+
+    ----------
+
+    size: int
+
+       The size of the square
+
+    """
     return json.dumps(my_obj)
