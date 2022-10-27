356b2d4eacba7c4c536584fef2263e3142d35da7
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-27 05:40:45 +0300

    json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
+"""class_to_json."""
 
 
 def class_to_json(obj):
+    """
+    class.
+
+    ...
+
+    Attributes
+    ----------
+    __width:
+        this is the width of the rectangle
+    __height:
+        this is the height of the rectangle
+
+    Methods
+    -------
+    None
+
+    """
     return vars(obj)
