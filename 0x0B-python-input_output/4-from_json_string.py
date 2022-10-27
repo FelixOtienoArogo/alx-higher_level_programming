335c3a5fc4b74393c4f6312b164ad56f5ce8eb4f
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-27 05:12:54 +0300

    from

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-
+"""to_json_string.py"""
 
 import json
 
 
-def from_json_string(my_str):
-    return json.loads(my_str)
+def from_json_string(my_obj):
+    """
+    Parameters.
+
+    ----------
+
+    size: int
+
+       The size of the square
+
+    """
+    return json.loads(my_obj)
