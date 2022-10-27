6168a93ccef7240262012d61f0c9ca31b08e0927
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-27 05:18:25 +0300

    save

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,19 @@
-
-
 #!/usr/bin/python3
-
+"""to_json_string.py"""
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    Parameters.
+
+    ----------
+
+    size: int
+
+       The size of the square
+
+    """
     with open(filename, "w", encoding="UTF-8") as f:
         json.dump(my_obj, f)
