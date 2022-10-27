19f2ba416e78b14fc00e62c1a3f3981aeb1cbf7d
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-27 05:08:18 +0300

    load

---
+++ 0x0B-python-input_output/4-form_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""to_json_string.py"""
+
+import json
+
+
+def from_json_string(my_obj):
+    """
+    Parameters.
+
+    ----------
+
+    size: int
+
+       The size of the square
+
+    """
+    return json.loads(my_obj)
