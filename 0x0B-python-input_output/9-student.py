1612b3f7444b68100f7932d306107e33c394a577
FelixOtienoArogo <felixarogo@gmail.com>
2022-10-27 05:47:15 +0300

    class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""class_to_json."""
+
+
+class Student:
+    """
+    class.
+
+    ...
+
+    Attributes
+    ----------
+    __width:
+        this is the width of the rectangle
+    __height:
+        this is the height of the rectangle
+
+    Methods
+    -------
+    None
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Properties.
+
+        __________
+
+        None
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Properties.
+
+        __________
+
+        None
+
+        """
+        return self.__dict__
