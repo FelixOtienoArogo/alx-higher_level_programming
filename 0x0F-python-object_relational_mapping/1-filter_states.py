2f533e47cb48c1d6fcb19c1a309b4d646eadddd5
FelixOtienoArogo <felixarogo@gmail.com>
2022-12-24 22:37:34 +0300

    f

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
                          user=sys.argv[1], password=sys.argv[2],
                          database=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE NAME LIKE 'N%'")
-    [print(state) for state in cursor.fetchall()]
+    cursor.execute("SELECT * FROM `states` WHERE NAME LIKE 'N%' ORDER BY `id`")
+    [print(state) for state in cursor.fetchall() if state[1][0] == "N"]
