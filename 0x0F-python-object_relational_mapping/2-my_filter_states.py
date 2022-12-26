44f0e7ae7d344ea351a03544e88db6cf43561e71
FelixOtienoArogo <felixarogo@gmail.com>
2022-12-26 13:42:37 +0300

    ar

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
                          user=sys.argv[1], password=sys.argv[2],
                          database=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM `states`\
+    WHERE BINARY `name` = '{}'".format(sys.argv[4]))
     [print(state) for state in cursor.fetchall()]
