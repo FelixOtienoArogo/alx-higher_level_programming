82f4551002bfa1765ec73b60a60d8d20e782c5d9
FelixOtienoArogo <felixarogo@gmail.com>
2022-12-26 20:19:52 +0300

    san

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,7 +4,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], host="localhost", port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
+                         database=sys.argv[3], host="localhost", port=3306)
     c = db.cursor()
     c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
     FROM `cities` as `c` \
