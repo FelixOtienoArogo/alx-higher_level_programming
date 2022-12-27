300be3be67933f434cfeab9145c011b70c7e58b8
FelixOtienoArogo <felixarogo@gmail.com>
2022-12-27 13:18:15 +0300

    fetch

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -3,9 +3,9 @@
 """
 import sys
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from urllib.parse import quote
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:%s@localhost/{}".format(sys.argv[1], sys.argv[3]) % quote(sys.argv[2]))
     Base.metadata.create_all(engine)
