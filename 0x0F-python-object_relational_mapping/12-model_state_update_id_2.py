6f769d5992c7884b07772613923395d635186039
FelixOtienoArogo <felixarogo@gmail.com>
2022-12-27 14:10:13 +0300

    update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all States objects from hbtn_0e_6_usa."""
+
+
+import sys
+from model_state import Base, State
+from urllib.parse import quote
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:%s@localhost/{}"
+                           .format(sys.argv[1], sys.argv[3]) %
+                           quote(sys.argv[2]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(id=2).first()
+    state.name='New Mexico'
+    session.commit()
